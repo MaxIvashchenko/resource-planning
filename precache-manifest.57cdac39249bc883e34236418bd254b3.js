0cd26998e948272a807c3ca5200730cd7cff628c
Ivashchenko Maksym <maksym.entex@gmail.com>
2020-09-15 17:47:48 +0300

    Updates

---
+++ precache-manifest.57cdac39249bc883e34236418bd254b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb67b87f46f25e5b6acd97c6194046c",
+    "url": "/resource-planning/index.html"
+  },
+  {
+    "revision": "e1e3ee91add424d04550",
+    "url": "/resource-planning/static/css/2.2f73783b.chunk.css"
+  },
+  {
+    "revision": "76f1cbf5dad6eef7513b",
+    "url": "/resource-planning/static/css/main.4f31ea91.chunk.css"
+  },
+  {
+    "revision": "e1e3ee91add424d04550",
+    "url": "/resource-planning/static/js/2.943e2582.chunk.js"
+  },
+  {
+    "revision": "43fafa47fcc31ad801a24b8595dc7aed",
+    "url": "/resource-planning/static/js/2.943e2582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76f1cbf5dad6eef7513b",
+    "url": "/resource-planning/static/js/main.6e11dc46.chunk.js"
+  },
+  {
+    "revision": "86f4444b25e9693af0a1",
+    "url": "/resource-planning/static/js/runtime-main.194fdda5.js"
+  },
+  {
+    "revision": "e52086342caab479aa753e633130c61c",
+    "url": "/resource-planning/static/media/buttonLeft.e5208634.svg"
+  },
+  {
+    "revision": "fb0a3ebd668cec8c4c654413f20fa336",
+    "url": "/resource-planning/static/media/buttonRight.fb0a3ebd.svg"
+  }
+]);
